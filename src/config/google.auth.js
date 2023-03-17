46a0c8571b319d8a784894a0a024d4bf28e91eee
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-17 19:47:37 +0530

    Create a arrow function with user information

--- src/config/google.auth.js
+++ src/config/google.auth.js
@@ -11,6 +11,8 @@ const googleAuth = (passport) => {
           clientID: config.GOOGLE_CLIENT_ID,
           clientSecret: config.GOOGLE_CLIENT_SECRET,
           callbackURL: config.GOOGLE_REDIRECT_URL,
-        },
+        }, (accessToken, refreshToken, profile, callback) => {
+
+        }
       ))
     }      
