8e2c698c9ec7ca2ae71f64e302d1e4d9cfb341ff
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-17 19:42:57 +0530

    Create google.auth.js file and imports configs and package to create authentication strategy

---
+++ src/config/google.auth.js
@@ -0,0 +1,16 @@
+import GoogleStrategy from "passport-google-oauth20";
+import config from ".";
+import logger from "../utils/logger";
+
+const googleAuth = (passport) => {
+    GoogleStrategy.Strategy;
+  
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: config.GOOGLE_CLIENT_ID,
+          clientSecret: config.GOOGLE_CLIENT_SECRET,
+          callbackURL: config.GOOGLE_REDIRECT_URL,
+        },
+      ))
+    }      
