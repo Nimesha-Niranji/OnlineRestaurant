730fb1a31a83a2444cef057679ab362c9e1e2b3d
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-18 22:52:03 +0530

    Enter icons

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -1,17 +1,40 @@
 import React/*, {useContext, useState}*/ from 'react'
 //import {GlobalState} from '../../GlobalState'
 import Menu from './icon/menu.svg'
-//import Close from './icon/close.svg'
+import Close from './icon/close.svg'
+import Cart from './icon/cart.svg'
+import {Link} from 'react-router-dom'
 
 function Header() {
     //const value = userContext(GlobalState)
   return (
-    //<header>
+    <header>
         <div className= "menu">
-          <img src={Menu} alt="" width="30" color='black'/>
-         
+          <img src={Menu} alt="" width="30"/>
         </div>
-    //</header>
+
+        <div className= "logo">
+          <h1>
+            <Link to="/">Foodie Lovers</Link>
+          </h1>
+        </div>
+
+        <ul>
+          <li><Link to = "/">Products</Link></li>
+          <li><Link to = "/login">Login / Register</Link></li>
+          <li>
+            <img src = {Close} alt="" width="30" />
+          </li>
+        </ul>
+
+        <div className = "cart-icon">
+          <span>0</span>
+          <Link>
+            <img src = {Cart} alt="" width = "30"/>
+          </Link>
+        </div>
+
+    </header>
   )
 }
 
