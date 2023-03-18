a4d3d9f016e587f8ef3b0f6856d764779693d6c6
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-18 14:21:12 +0530

    Code the userCtrl.js file

--- src/api/controllers/userCtrl.js
+++ src/api/controllers/userCtrl.js
@@ -1,8 +1,46 @@
-const userCtrl = {
-    register: (req, res) => {
-        res.json({msg : "Test Controllers"})
-    }
+const Users = require('../model/userModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
+const userCtrl = {
+    register: async (req, res) =>{
+        try {
+            const {name, email, password} = req.body;
+
+            const user = await Users.findOne({email})
+            if(user) return res.status(400).json({msg: "The email already exists."})
+
+            if(password.length < 6) 
+                return res.status(400).json({msg: "Password is at least 6 characters long."})
+
+            // Password Encryption
+            const passwordHash = await bcrypt.hash(password, 10)
+            const newUser = new Users({
+                name, email, password: passwordHash
+            })
+
+            // Save mongodb
+            await newUser.save()
+
+            // Then create jsonwebtoken to authentication
+            const accesstoken = createAccessToken({id: newUser._id})
+
+            //res.json({msg : "Resgister Success!"})    
+
+            res.json({accesstoken})
+
+} catch (err) {
+            return res.status(500).json({msg: err.message})
+        
+}
+
+}
+
+
+}
+
+const createAccessToken = (user) =>{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
 }
 
 module.exports = userCtrl
