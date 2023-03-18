64896b7d8f5c4232ecf530718cf361ca2b097511
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-18 23:36:23 +0530

    Add login, register, cart, NotFound to Pages.js file

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Products from './products/Products'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Cart from './cart/Cart'
+import NotFound from './utils/not_found/NotFound'
 
 function Pages() {
   return (
-    <div>
-      main pages components
-    </div>
+    <switch>
+      <Route path="/" exact component={Products} />
+      <Route path="/login" exact component={isLogged ? NotFound : Login} />
+      <Route path="/register" exact component={isLogged ? NotFound : Register} />
+      <Route path="/cart" exact component={Cart} />
+      <Route path="*" exact component={NotFound} />
+    </switch>
   )
 }
 
