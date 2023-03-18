f72cfc6dc7795678cb3f1341463832c0e2b741f3
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-18 18:25:49 +0530

    Create delete image function

--- src/api/routes/upload.js
+++ src/api/routes/upload.js
@@ -42,6 +42,24 @@ router.post('/upload',auth , authAdmin, (req, res) =>{
     }
 })
 
+// Delete image
+router.post('/destroy',auth , authAdmin, (req, res) =>{
+    try {
+        const {public_id} = req.body;
+        if(!public_id) return res.status(400).json({msg: 'No images Selected'})
+
+        cloudinary.v2.uploader.destroy(public_id, async(err, result) =>{
+            if(err) throw err;
+
+            res.json({msg: "Deleted Image"})
+        })
+
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+    
+})
+
 const removeTmp = (path) =>{
     fs.unlink(path, err=>{
         if(err) throw err;
