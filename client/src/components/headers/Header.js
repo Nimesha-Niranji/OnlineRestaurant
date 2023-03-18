f3ff84cc403e2482dd4686f4b6705f87c30e94f2
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-18 22:31:17 +0530

    Check the menu icon on the screen

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React/*, {useContext, useState}*/ from 'react'
+//import {GlobalState} from '../../GlobalState'
+import Menu from './icon/menu.svg'
+//import Close from './icon/close.svg'
 
 function Header() {
+    //const value = userContext(GlobalState)
   return (
-    <div>
-      header components
-    </div>
+    //<header>
+        <div className= "menu">
+          <img src={Menu} alt="" width="30" color='black'/>
+         
+        </div>
+    //</header>
   )
 }
 
