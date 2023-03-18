b39e59bfc8bc16a1f4af42cff1dd7e4d26349d08
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-18 10:30:19 +0530

    Create user information

--- src/config/google.auth.js
+++ src/config/google.auth.js
@@ -1,7 +1,7 @@
 import GoogleStrategy from "passport-google-oauth20";
 import config from ".";
 import User from "../api/model/user.model";
-import logger from "../utils/logger";
+//import logger from "../utils/logger";
 
 const googleAuth = (passport) => {
     GoogleStrategy.Strategy;
@@ -13,7 +13,7 @@ const googleAuth = (passport) => {
           clientSecret: config.GOOGLE_CLIENT_SECRET,
           callbackURL: config.GOOGLE_REDIRECT_URL,
         }, 
-        (accessToken, refreshToken, profile, callback) => {
+        async(accessToken, refreshToken, profile, callback) => {
           const userObj = {
             googleId: profile.id,
             displayName: profile.displayName,
@@ -21,8 +21,21 @@ const googleAuth = (passport) => {
             image: profile.photos[0].value,
             firstName: profile.name.givenName,
             lastName: profile.name.familyName,
-          }
-            return callback(null, profile);
+          };
+
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (user) {
+          return callback(null, user);
+        }
+
+        User.create(userObj)
+          .then((user) => {
+            return callback(null, user);
+          })
+          .catch((err) => {
+            return callback(err.message);
+          });
         }
     ));
 
@@ -31,7 +44,9 @@ const googleAuth = (passport) => {
       });
     
     passport.deserializeUser((id, callback) => {
-        callback(null, id);
+        user.findById(id, (err, user) => {
+          callback(err, user);
+        })
         
       });
 
