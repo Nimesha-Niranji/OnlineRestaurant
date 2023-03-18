bb700bb2e9b6c79dfd3ad5d461480e73bc78bd3e
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-18 11:36:56 +0530

    Create user page

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,15 +1,20 @@
-const routesInit = (app, passport) => {
-    app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-    app.get(
-      "/auth/google/callback",
-      passport.authenticate("google", {
-        failureRedirect: "/login",
-      }),
-      (req, res) => {
-        console.log("User authenticated");
-      }
-    );
-    
-  };
+import { authenticate } from "../middleware/auth.middleware";
 
-  export {routesInit};
+const routesInit = (app, passport) => {
+  app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", {
+      failureRedirect: "/login",
+      successRedirect: "/user",
+    }),
+    (req, res) => {
+      console.log("User authenticated");
+    }
+  );
+  app.get("/test", authenticate, (req, res) => {
+    res.send("<h3>User is authenticated</h3>");
+  });
+};
+
+export { routesInit };
